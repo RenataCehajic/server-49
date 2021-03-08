482b56bbd8aeb21702da247b461178fb07991594
RenataCehajic <renata.cehajic@gmail.com>
2021-03-08 16:30:54 +0100

    Populate 3 databases with data

---
+++ seeders/20210308151437-some-todoLists.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "todoLists",
+      [
+        {
+          name: "Pick up Emma",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Do exercises",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("todoLists", null, {});
+  },
+};
---
+++ seeders/20210308152710-some-todoItems.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "todoItems",
+      [
+        {
+          task: "Get some fresh air",
+          deadline: "today",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          task: "Medidate",
+          deadline: "tomorrow",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("todoItems", null, {});
+  },
+};
